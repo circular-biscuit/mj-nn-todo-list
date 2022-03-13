a1812b6f4f9ce1acdeac4b7bc5f61256665ca9d6
circular-biscuit <mrdeb99@gmail.com>
2022-03-13 20:54:05 +0530

    search and filter functionality

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const form = document.querySelector('.add');
 const list = document.querySelector('.list-group');
+const search = document.querySelector('.search input');
 
 //add todos
 form.addEventListener('submit', e => {
@@ -17,11 +18,25 @@ form.addEventListener('submit', e => {
         list.innerHTML += html;
         form.reset(); //to clear form for the next entries
     }
-})
+});
 
 //delete todos
 list.addEventListener('click', e => {
     if (e.target.classList.contains('delete')) {
         e.target.parentNode.remove();
     }
-})
+});
+
+//search for todos
+search.addEventListener('keyup', () => {
+    const searchTerm = search.value.trim().toLowerCase(); //current user input search term
+    //if todo doesn't include search term, hide it
+    Array.from(list.children) //convert to do list from html collection into array
+        .map(todo => {
+            if (!todo.textContent.toLowerCase().includes(searchTerm)){
+                todo.classList.add('d-none');
+            } else {
+                todo.classList.remove('d-none');
+            }
+        })
+});
